529d43f30296e7300d31b0897b117a47917d6266
Mikhail Martynenko <2424324@gmail.com>
2020-05-08 23:35:30 +0300

    modified:   app.py

--- app/app.py
+++ app/app.py
@@ -10,8 +10,8 @@ from celery import Celery
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
 
 
 def make_celery(app):
