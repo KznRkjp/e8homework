18c4de517e43234b875bf1278e783a316167e899
KznRkjp <mikhail@Mikhails-MacBook-Pro.local>
2020-05-11 18:46:47 +0300

    one more try

--- app/app.py
+++ app/app.py
@@ -10,6 +10,8 @@ from celery import Celery
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['CELERY_BROKER_URL'] = 'redis://192.168.1.79:6379'
+#app.config['CELERY_RESULT_BACKEND'] = 'redis://192.168.1.79:6379'
 app.config['CELERY_BROKER_URL'] = 'redis://redis:6379'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379'
 
@@ -36,8 +38,8 @@ celery = make_celery(app)
 #celery -A your_application.celery worker
 
 db = SQLAlchemy(app)
-#celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-#celery.conf.update(app.config)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 class Results(db.Model):
